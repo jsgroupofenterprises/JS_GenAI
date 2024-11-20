61e16842e783238b5f5ea1f9a807c8d23d0a156e
jassimshanavaskhan <jassimshanavaskhan@gmail.com>
2024-11-20 21:31:04 +0530

    added import os in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS  # Add this import
 from RDKAssistant_Class import RDKAssistant
+import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS
 
